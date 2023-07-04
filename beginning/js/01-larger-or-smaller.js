05cd55f08cfa0fc4229c1cf468e508d7fcff421c
Jess Rude <jessicajoyrude@gmail.com>
2023-07-03 22:03:17 -0700

    testing1233

--- beginning/js/01-larger-or-smaller.js
+++ beginning/js/01-larger-or-smaller.js
@@ -1,3 +1,10 @@
-Larger or Smaller?
-Usage: Use simple conditional statements
-Create an application that accepts two integers within two separate prompts. Then, display only the larger of the two within the browser window. Don’t forget to handle the fact that the two could be equal.
+// Usage: Use simple conditional statements
+//Create an application that accepts two integers within two separate prompts. Then, display only the larger of the two within the browser window. Don’t forget to handle the fact that the two could be equal.
+
+
+let x = window.prompt("Enter a number between 1 and 10") + window.prompt("Enter a number between 1 and 10");
+if (x == 5) 
+    window.alert("It's bigger");
+     else {
+    window.alert("They are equal!");
+}
---
+++ beginning/js/02-word-translator.js
@@ -0,0 +1,10 @@
+// Create an application that prompts the user for a language code ("es", "de", "en", and "fr"). If they enter one of the four codes, display the translated version of "Hello World" for the given language within the console window. If the user enters anything but those four language codes, the application should default to English. The result should be (for example):
+
+// Hello World translated in French is: Bonjour le monde
+
+
+let tech = window.prompt("Which language?");
+switch (en) {case "html":window.alert("Hello World");
+break;case "css":window.alert("CSS is more difficult to pick up than HTML!");
+break;default:window.alert("Hola World");
+}
