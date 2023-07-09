90b45ac94460523b7c31ca64f0d8c126c8349729
Jess Rude <jessicajoyrude@gmail.com>
2023-07-08 18:00:23 -0700

    exercise 1

--- beginning/js/01-larger-or-smaller.js
+++ beginning/js/01-larger-or-smaller.js
@@ -1,10 +1,17 @@
-// Usage: Use simple conditional statements
-//Create an application that accepts two integers within two separate prompts. Then, display only the larger of the two within the browser window. Don’t forget to handle the fact that the two could be equal.
-
-
-let x = window.prompt("Enter a number between 1 and 10") + window.prompt("Enter a number between 1 and 10");
-if (x == 5) 
-    window.alert("It's bigger");
-     else {
-    window.alert("They are equal!");
-}
+let num1, num2;
+num1 = window.prompt("Provide a number", "0");
+num2 = window.prompt("Provide a second", "0");
+                                                 
+if(parseInt(num1, 10) > parseInt(num2, 10)) 
+  { 
+  console.log("The larger of "+ num1+ " and "+ num2+ " is "+ num1+ ".");
+  }   
+else
+  if(parseInt(num2, 10) > parseInt(num1, 10)) 
+  {
+  console.log("The larger of "+ num1+" and "+ num2+ " is "+ num2+ ".");
+  }                  
+else
+  {
+   console.log("The values "+ num1+ " and "+num2+ " are equal.");
+  }
