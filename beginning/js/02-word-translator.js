cb2e54d01a5f7f42ace4d329e28411cfb241a037
Jess Rude <jessicajoyrude@gmail.com>
2023-07-08 18:19:11 -0700

    exercise 2

--- beginning/js/02-word-translator.js
+++ beginning/js/02-word-translator.js
@@ -1,10 +1,17 @@
-// Create an application that prompts the user for a language code ("es", "de", "en", and "fr"). If they enter one of the four codes, display the translated version of "Hello World" for the given language within the console window. If the user enters anything but those four language codes, the application should default to English. The result should be (for example):
-
-// Hello World translated in French is: Bonjour le monde
-
-
-let tech = window.prompt("Which language?");
-switch (en) {case "html":window.alert("Hello World");
-break;case "css":window.alert("CSS is more difficult to pick up than HTML!");
-break;default:window.alert("Hola World");
+let language = prompt('("es", "de", "en", and "fr"');
+switch (language) {
+case "es":
+document.write("Hola Mundo");
+break;
+case "de":
+document.write("Hallo Wereld");
+break;
+case "en":
+document.write("Hello World");
+break;
+case "fr":
+document.write("Bonjour le monde");
+break;
+default:
+document.write("Hello World!");
 }
