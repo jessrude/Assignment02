049dadb58c6492c7f33ccd4da74e0ea35a0bd3bb
Jess Rude <jessicajoyrude@gmail.com>
2023-07-09 23:13:46 -0700

    updates

--- beginning/js/05-hitchhikers-guide.js
+++ beginning/js/05-hitchhikers-guide.js
@@ -1,19 +1,37 @@
-Usage: Use a combination of simple conditional, nested if/else if, and switch statements
-In this classic “Hitchhiker’s Guide to the Galaxy” game, you will build an application that begins the user on a journey where the path they take can determine the outcome of the player. To complete the game follow the steps outlined below:
-1. Prompt the user and ask them if they are ready to play the game. If they say “yes”, continue. If they say “no” or anything other than “no”, thank them and ask them to play again sometime.
-2. Alert the user within the following:
+//I am not sure how to connect part 1 to part 2
 
-You are in a dark, dingy, and humid cave searching for the lost treasure of Captain Chingadera. You are disoriented, lost, hungry and extremely thirsty. You see a speck of light in the distance ahead of you, something shimmering to your right, and the sound of running water to your left. Your back is against the wall.
-3. Then prompt the user with the following:
+//part 1
+game = prompt('"Ready to play a game? Type "Yes" if you are!');
+switch (game) {
+case "Yes":
+document.write("You are in a dark, dingy, and humid cave searching for the lost treasure of Captain Chingadera. You are disoriented, lost, hungry and extremely thirsty. You see a speck of light in the distance ahead of you, something shimmering to your right, and the sound of running water to your left. Your back is against the wall.");
+break;
+case "No":
+document.write("That's okay. Let's play another time!");
+break;
+default:
+document.write("That's okay. Let's play another time!");
+break;}
 
-Which direction would you like to head (please enter forward, left, or right).
-4. Using a switch statement, evaluate the direction that the user selected.
+//part 2
+options = prompt('Which direction would you like to head (please enter forward, left, or right)?');
+switch (options) {
+case "left":
+document.write("Your thirst has gotten the better of you. You trip on a rock, hit your head, and fall into a pool of water and drown!");
+break;
+case "forward":
+document.write("You walk 100 yards and safely make your way out of the cave.");
+break;
+case "right":
+document.write("You found the gold! You walk into a small room and see thousands of gold coins, jewels, chalices, and more. You jump into the pile of gold in celebration and immediately a hidden door slams down and traps you in the room forever!");
+break;
+default:
+document.write("The ghost of Captain Chingadera has condemned you to eternal damnation and you shall now burn in the hot excoriation for lifeless lowlifes for not choosing the correct option.");
+}
 
-left: alert, Your thirst has gotten the better of you. You trip on a rock, hit your head, and fall into a pool of water and drown!
-forward: alert, You walk 100 yards and safely make your way out of the cave.
-right: alert, You found the gold! You walk into a small room and see thousands of gold coins, jewels, chalices, and more. You jump into the pile of gold in celebration and immediately a hidden door slams down and traps you in the room forever!
- 
-5. If the user selects anything other than forward, left, or right alert the user 
-
-The ghost of Captain Chingadera has condemned you to eternal damnation and you shall now burn in the hot excoriation for lifeless lowlifes for not choosing the correct option.
-6. The game is now over. Ask the user to rate the game between 1 and 10. If the user enters a number not between 1 and 10, thank the user for playing and end the game, If the user enters a number between 6 and 10, thank the user and ask them to play again. If the user enters a number between 1 and 5, thank the user and tell them that you are working hard to improve the game.
+let num = Number(prompt(`GAME OVER. Please rate the game between 1 and 10.`))
+if (num <= 5) {
+   console.log (`Thank you for your feedback, we are working on improving the game.`)
+    } else { 
+   console.log(`Thank you for your feedback and please play again soon!`)
+   }
