4263f631f305049ce27c7fbf93e9312507666657
Jess Rude <jessicajoyrude@gmail.com>
2023-07-09 19:30:21 -0700

    coin flip updates

--- beginning/js/03-coinflip.js
+++ beginning/js/03-coinflip.js
@@ -1,20 +1,22 @@
-let randomNum = Math.round(Math.random(.5));
+alert(`Flip a coin!`)
+let choice = prompt(`Heads or tails?`)
+
+let randomNum = Math.round(Math.random());
 let coinFlip = randomNum;
 
-alert(`Let\`s flip a coin.`)
-let choice = prompt(`Heads or tails?`)
+
 
 switch (choice) {
     case `heads`:
-        console.log(`The flip was heads and you chose heads...you win!`)
-        break
-    case `tails`:
-       console.log (`The flip was heads but you chose tails...you lose!`)
-       break
-    case `heads`:
-        console.log(`The flip was tails but you chose heads...you lose!`)
-        break
-    case `tails`:
-       console.log (`The flip was tails and you chose tails...you win!`)
-       break
+    if (randomNum < 0.5) {
+    console.log("The flip was heads and you chose heads...you win!");
+    }if (randomNum > 0.5) {
+    console.log("The flip was tails but you chose heads...you lose!");
 }
+break
+case `tails`:
+    if (randomNum > 0.5) {
+    console.log("The flip was tails and you chose tails...you win!");
+ }if (randomNum < 0.5) {
+    console.log("The flip was heads but you chose tails...you lose!");
+}}
